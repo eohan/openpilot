1c43baa97576c66a36a6a7695dd73ca7709bad6b
Mike Smith <DrZiplok@me.com>
2011-07-14 21:43:04 -0700

    Switch the clock config over to the external oscillator.  This gets code running through clock init to the FreeRTOS scheduler.

--- flight/PX2IO/System/pios_board.c
+++ flight/PX2IO/System/pios_board.c
@@ -27,9 +27,9 @@
  * Clocking
  */
 const struct pios_clock_cfg px2io_clock_config = {
-		.source				= RCC_PLLSource_HSI_Div2,	/* XXX temporary until we sort out the clock on IO */
+		.source				= RCC_PLLSource_PREDIV1,	/* XXX temporary until we sort out the clock on IO */
 		.refclock_frequency = 8 * 1000 * 1000,
-		.refclock_prescale	= RCC_PREDIV1_Div2,
+		.refclock_prescale	= RCC_PREDIV1_Div4,
 		.pll_multiply		= RCC_PLLMul_6,
 		.hclk_prescale		= RCC_SYSCLK_Div1,
 		.pclk1_prescale		= RCC_HCLK_Div1,
