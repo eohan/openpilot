de1526d7421c2d4b56f1a1af73229bb4d30fecb8
Mike Smith <DrZiplok@me.com>
2011-07-17 23:19:30 -0700

    Allocate 4K for the PX2IO bootloader

--- make/boards/px2io/board-info.mk
+++ make/boards/px2io/board-info.mk
@@ -11,13 +11,12 @@ BOARD               := STM32F100C6T6B_PX2IO_Rev1
 MODEL               := MD
 MODEL_SUFFIX        :=
 
-# Note, osc freq will probably change with a board revision
+# Note, osc freq may change with a board revision
 OSCILLATOR_FREQ     := 8000000
 SYSCLK_FREQ         := 24000000
 
 # Note: These must match the values in link_$(BOARD)_memory.ld
-# XXX currently not supporting a bootloader
-FW_BANK_BASE        := 0x08000000   # Start of firmware flash
-FW_BANK_SIZE        := (32 * 1024)  # Should include FW_DESC_SIZE
+FW_BANK_BASE        := 0x08001000	# Start of firmware flash
+FW_BANK_SIZE        := 0x7000		# Should include FW_DESC_SIZE
 
 FW_DESC_SIZE        := 0x00000064
