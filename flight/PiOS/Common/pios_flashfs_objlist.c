c0afe9400b8433d1aa5d3986deb78a472cc9f1c9
LM <lm@student.ethz.ch>
2011-11-14 09:46:51 +0100

    Changed object magic, forcing a settings reset

--- flight/PiOS/Common/pios_flashfs_objlist.c
+++ flight/PiOS/Common/pios_flashfs_objlist.c
@@ -56,8 +56,8 @@ struct fileHeader {
 } __attribute__((packed));
 
 
-#define OBJECT_TABLE_MAGIC 0x85FB3C35
-#define OBJ_MAGIC          0x3015AE71
+#define OBJECT_TABLE_MAGIC 0x85FB3C38
+#define OBJ_MAGIC          0x3015AE75
 #define OBJECT_TABLE_START 0x00000010
 #define OBJECT_TABLE_END   0x00001000
 #define MAX_BADMAGIC       4
